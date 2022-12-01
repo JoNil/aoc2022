4581eeefd76a1ebf932e032f9ecebd864d8871d1
Jonathan Nilsson <jonathan@voysys.se>
2022-12-01 10:55:55 +0100

    Day 1

---
+++ src/day1.rs
@@ -0,0 +1,54 @@
+pub static INPUT: &str = include_str!("input/1.txt");
+
+pub fn a(input: &str) -> i32 {
+    input
+        .split("\n\n")
+        .map(|elf| {
+            elf.lines()
+                .map(|cal| cal.parse::<i32>().unwrap())
+                .sum::<i32>()
+        })
+        .max()
+        .unwrap()
+}
+
+#[test]
+fn test_a() {
+    assert_eq!(a(TEST_INPUT), 24000);
+    assert_eq!(a(INPUT), 69310);
+}
+
+pub fn b(input: &str) -> i32 {
+    let mut sum_of_elfs = input
+        .split("\n\n")
+        .map(|elf| {
+            elf.lines()
+                .map(|cal| cal.parse::<i32>().unwrap())
+                .sum::<i32>()
+        })
+        .collect::<Vec<_>>();
+
+    sum_of_elfs.sort_by(|a, b| b.partial_cmp(a).unwrap());
+    sum_of_elfs.iter().take(3).sum::<i32>()
+}
+
+#[test]
+fn test_b() {
+    assert_eq!(b(TEST_INPUT), 45000);
+    assert_eq!(b(INPUT), 206104);
+}
+
+static TEST_INPUT: &str = "1000
+2000
+3000
+
+4000
+
+5000
+6000
+
+7000
+8000
+9000
+
+10000";
---
+++ src/main.rs
@@ -0,0 +1,6 @@
+mod day1;
+
+fn main() {
+    println!("{}", day1::a(day1::INPUT));
+    println!("{}", day1::b(day1::INPUT));
+}
