ee726cd9694df49b289d05841586ced39f026fc1
Jonathan Nilsson <jonathan@voysys.se>
2022-12-02 08:53:08 +0100

    Day2

--- src/days/day2.rs
+++ src/days/day2.rs
@@ -2,21 +2,49 @@ pub static INPUT: &str = include_str!("../input/2.txt");
 pub static TEST_INPUT: &str = include_str!("../input/2_test.txt");
 
 pub fn a(input: &str) -> i32 {
-    0
+    input
+        .lines()
+        .map(|line| match line {
+            "A X" => 1 + 3,
+            "A Y" => 2 + 6,
+            "A Z" => 3,
+            "B X" => 1,
+            "B Y" => 2 + 3,
+            "B Z" => 3 + 6,
+            "C X" => 1 + 6,
+            "C Y" => 2,
+            "C Z" => 3 + 3,
+            _ => panic!("{line}"),
+        })
+        .sum::<i32>()
 }
 
 #[test]
 fn test_a() {
-    assert_eq!(a(TEST_INPUT), 0);
-    assert_eq!(a(INPUT), 0);
+    assert_eq!(a(TEST_INPUT), 15);
+    assert_eq!(a(INPUT), 13005);
 }
 
 pub fn b(input: &str) -> i32 {
-    0
+    input
+        .lines()
+        .map(|line| match line {
+            "A X" => 3,
+            "A Y" => 1 + 3,
+            "A Z" => 2 + 6,
+            "B X" => 1,
+            "B Y" => 2 + 3,
+            "B Z" => 3 + 6,
+            "C X" => 2,
+            "C Y" => 3 + 3,
+            "C Z" => 1 + 6,
+            _ => panic!("{line}"),
+        })
+        .sum::<i32>()
 }
 
 #[test]
 fn test_b() {
-    assert_eq!(b(TEST_INPUT), 0);
-    assert_eq!(b(INPUT), 0);
+    assert_eq!(b(TEST_INPUT), 12);
+    assert_eq!(b(INPUT), 11373);
 }
