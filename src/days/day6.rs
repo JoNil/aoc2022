190dd07072801fe060af4c96446ca69156620f63
Jonathan Nilsson <jonathan@voysys.se>
2022-12-06 08:12:55 +0100

    Day 6 a

---
+++ src/days/day6.rs
@@ -0,0 +1,42 @@
+use std::collections::HashSet;
+
+pub static INPUT: &str = include_str!("../input/6.txt");
+pub static TEST_INPUT_1: &str = include_str!("../input/6_test_1.txt");
+pub static TEST_INPUT_2: &str = include_str!("../input/6_test_2.txt");
+pub static TEST_INPUT_3: &str = include_str!("../input/6_test_3.txt");
+pub static TEST_INPUT_4: &str = include_str!("../input/6_test_4.txt");
+pub static TEST_INPUT_5: &str = include_str!("../input/6_test_5.txt");
+
+pub fn a(input: &str) -> i32 {
+    input
+        .as_bytes()
+        .windows(4)
+        .enumerate()
+        .find(|(_, w)| w.iter().collect::<HashSet<_>>().len() == 4)
+        .map(|(count, _)| count + 4)
+        .unwrap() as i32
+}
+
+#[test]
+fn test_a() {
+    assert_eq!(a(TEST_INPUT_1), 5);
+    assert_eq!(a(TEST_INPUT_2), 6);
+    assert_eq!(a(TEST_INPUT_3), 10);
+    assert_eq!(a(TEST_INPUT_4), 11);
+    assert_eq!(a(TEST_INPUT_5), 7);
+    assert_eq!(a(INPUT), 1175);
+}
+
+pub fn b(input: &str) -> i32 {
+    0
+}
+
+#[test]
+fn test_b() {
+    assert_eq!(a(TEST_INPUT_1), 5);
+    assert_eq!(a(TEST_INPUT_2), 6);
+    assert_eq!(a(TEST_INPUT_3), 10);
+    assert_eq!(a(TEST_INPUT_4), 11);
+    assert_eq!(a(TEST_INPUT_5), 7);
+    assert_eq!(b(INPUT), 0);
+}
