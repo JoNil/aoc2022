8349a414675d46c745ba520cd4e1950755d3bf2b
Jonathan <jonathan@voysys.se>
2022-12-09 15:10:25 +0100

    Den är fel

--- src/days/day9.rs
+++ src/days/day9.rs
@@ -1,27 +1,43 @@
-use glam::ivec2;
+use std::collections::HashSet;
+
+use glam::{ivec2, IVec2};
 
 pub static INPUT: &str = include_str!("../input/9.txt");
 pub static TEST_INPUT: &str = include_str!("../input/9_test.txt");
 
+fn parse_dir(d: &str) -> IVec2 {
+    match d {
+        "R" => ivec2(1, 0),
+        "L" => ivec2(-1, 0),
+        "U" => ivec2(0, 1),
+        "D" => ivec2(0, -1),
+        _ => panic!("Invalid dir"),
+    }
+}
+
 pub fn a(input: &str) -> i32 {
-    let head = ivec2(0, 0);
-    let tail = ivec2(0, 0);
+    let mut head = ivec2(0, 0);
+    let mut tail = ivec2(0, 0);
+    let mut tail_pos = HashSet::new();
 
     for line in input.lines() {
-        match line
+        let (dir, count) = line
             .split_once(' ')
-            .map(|(d, s)| (d, s.parse::<i32>().unwrap()))
-            .unwrap()
-        {
-            ("R", s) => {}
-            ("L", s) => {}
-            ("U", s) => {}
-            ("D", s) => {}
-            _ => panic!(),
+            .map(|(d, s)| (parse_dir(d), s.parse::<i32>().unwrap()))
+            .unwrap();
+
+        for _ in 0..count {
+            if head * dir.abs() == tail * dir.abs() {
+                head += dir;
+            } else {
+                head += dir;
+                tail = head - dir;
+            }
+            tail_pos.insert(tail);
         }
     }
 
-    0
+    tail_pos.len() as i32
 }
 
 #[test]
