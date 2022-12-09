84c16be00535e714cb8f256d8d46973c6631a3e1
Jonathan <jonathan@voysys.se>
2022-12-09 15:45:06 +0100

    Hmmmm

--- src/days/day9.rs
+++ src/days/day9.rs
@@ -4,6 +4,7 @@ use glam::{ivec2, IVec2};
 
 pub static INPUT: &str = include_str!("../input/9.txt");
 pub static TEST_INPUT: &str = include_str!("../input/9_test.txt");
+pub static TEST_INPUT_2: &str = include_str!("../input/9_test_2.txt");
 
 fn parse_dir(d: &str) -> IVec2 {
     match d {
@@ -45,11 +46,35 @@ fn test_a() {
 }
 
 pub fn b(input: &str) -> i32 {
-    0
+    let mut tail_pos = HashSet::new();
+    let mut rope = [ivec2(0, 0); 10];
+
+    for line in input.lines() {
+        let (dir, count) = line
+            .split_once(' ')
+            .map(|(d, s)| (parse_dir(d), s.parse::<i32>().unwrap()))
+            .unwrap();
+
+        for _ in 0..count {
+            rope[0] += dir;
+            for i in 1..10 {
+                let head = rope[i - 1];
+                let tail = &mut rope[i];
+
+                if (head.x - tail.x).abs() > 1 || (head.y - tail.y).abs() > 1 {
+                    *tail = head - dir;
+                }
+            }
+            tail_pos.insert(rope[9]);
+        }
+    }
+
+    tail_pos.len() as i32
 }
 
 #[test]
 fn test_b() {
-    assert_eq!(b(TEST_INPUT), 0);
+    assert_eq!(b(TEST_INPUT), 1);
+    assert_eq!(b(TEST_INPUT_2), 36);
     assert_eq!(b(INPUT), 0);
 }
