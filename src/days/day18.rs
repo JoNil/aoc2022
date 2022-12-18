030e425417fa4de5dc4e2e23dd0b92f357362619
Jonathan Nilsson <jonathan@voysys.se>
2022-12-18 15:11:50 +0100

    Not so pretty

--- src/days/day18.rs
+++ src/days/day18.rs
@@ -1,9 +1,19 @@
 use std::collections::HashSet;
 
 use glam::{ivec3, IVec3};
+use pathfinding::prelude::dijkstra;
 
 pub static INPUT: &str = include_str!("../input/18.txt");
 pub static TEST_INPUT: &str = include_str!("../input/18_test.txt");
+
+static DIRS: &[IVec3] = &[
+    ivec3(1, 0, 0),
+    ivec3(-1, 0, 0),
+    ivec3(0, 1, 0),
+    ivec3(0, -1, 0),
+    ivec3(0, 0, 1),
+    ivec3(0, 0, -1),
+];
 
 fn parse(input: &str) -> HashSet<IVec3> {
     input
@@ -22,18 +32,7 @@ pub fn a(input: &str) -> i32 {
     let drop = parse(input);
 
     drop.iter()
-        .flat_map(|p| {
-            [
-                ivec3(1, 0, 0),
-                ivec3(-1, 0, 0),
-                ivec3(0, 1, 0),
-                ivec3(0, -1, 0),
-                ivec3(0, 0, 1),
-                ivec3(0, 0, -1),
-            ]
-            .into_iter()
-            .map(|c| c + *p)
-        })
+        .flat_map(|p| DIRS.iter().map(|c| *c + *p))
         .filter(|p| !drop.contains(p))
         .count() as i32
 }
@@ -45,34 +44,49 @@ fn test_a() {
 }
 
 pub fn b(input: &str) -> i32 {
-    let drop = parse(input);
+    let mut drop = parse(input);
+    let mut has_path_to_outside = HashSet::new();
+
+    loop {
+        let mut new_drop = drop.clone();
+        let mut has_tested = HashSet::new();
+
+        for candidate in drop.iter().flat_map(|p| DIRS.iter().map(|c| *c + *p)) {
+            if has_tested.contains(&candidate) {
+                continue;
+            }
+
+            if has_path_to_outside.contains(&candidate) {
+                continue;
+            }
+
+            has_tested.insert(candidate);
+
+            let path = dijkstra(
+                &candidate,
+                |&p| {
+                    DIRS.iter()
+                        .map(move |c| (*c + p, 1))
+                        .filter(|(p, _)| !drop.contains(p))
+                },
+                |&p| p == ivec3(-1, -1, -1),
+            );
+            if path.is_none() {
+                new_drop.insert(candidate);
+            } else {
+                has_path_to_outside.insert(candidate);
+            }
+        }
+
+        if drop.len() == new_drop.len() {
+            break;
+        }
+
+        drop = new_drop;
+    }
 
     drop.iter()
-        .flat_map(|p| {
-            [
-                ivec3(1, 0, 0),
-                ivec3(-1, 0, 0),
-                ivec3(0, 1, 0),
-                ivec3(0, -1, 0),
-                ivec3(0, 0, 1),
-                ivec3(0, 0, -1),
-            ]
-            .into_iter()
-            .map(|c| c + *p)
-        })
-        .filter(|p| {
-            ![
-                ivec3(1, 0, 0),
-                ivec3(-1, 0, 0),
-                ivec3(0, 1, 0),
-                ivec3(0, -1, 0),
-                ivec3(0, 0, 1),
-                ivec3(0, 0, -1),
-            ]
-            .into_iter()
-            .map(|c| c + *p)
-            .all(|p| drop.contains(&p))
-        })
+        .flat_map(|p| DIRS.iter().map(|c| *c + *p))
         .filter(|p| !drop.contains(p))
         .count() as i32
 }
@@ -80,5 +94,5 @@ pub fn b(input: &str) -> i32 {
 #[test]
 fn test_b() {
     assert_eq!(b(TEST_INPUT), 58);
-    assert_eq!(b(INPUT), 0);
+    assert_eq!(b(INPUT), 2000);
 }
