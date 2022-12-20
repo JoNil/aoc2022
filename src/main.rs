2b842cea713c32e711cb7e3e54c00ec40fe8d98a
Jonathan Nilsson <jonathan@voysys.se>
2022-12-20 09:11:31 +0100

    Only real

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,5 @@
 use aoc2022::day19;
 
 fn main() {
-    println!("{}", day19::b(day19::TEST_INPUT));
     println!("{}", day19::b(day19::INPUT));
-    println!("{}", day19::b2(day19::TEST_INPUT));
-    println!("{}", day19::b2(day19::INPUT));
 }
