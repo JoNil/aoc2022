f0fae1206bb06786a2338b0b891401d8d42306a8
Jonathan Nilsson <jonathan@voysys.se>
2022-12-21 21:03:56 +0100

    With assert

--- src/days/day20.rs
+++ src/days/day20.rs
@@ -3,6 +3,22 @@ use std::cmp::Ordering;
 pub static INPUT: &str = include_str!("../input/20.txt");
 pub static TEST_INPUT: &str = include_str!("../input/20_test.txt");
 
+fn modulo(a: i32, b: usize) -> usize {
+    let mut res = a % b as i32;
+    while res < 0 {
+        res += b as i32;
+    }
+    res as usize
+}
+
+#[test]
+fn test_modulo() {
+    assert_eq!(modulo(8, 4), 0);
+    assert_eq!(modulo(9, 4), 1);
+    assert_eq!(modulo(-1, 4), 3);
+    assert_eq!(modulo(-2, 4), 2);
+}
+
 pub fn a(input: &str) -> i32 {
     let original_file = input
         .lines()
@@ -10,9 +26,26 @@ pub fn a(input: &str) -> i32 {
         .collect::<Vec<_>>();
 
     let mut scrambled_file = original_file.clone();
+    let len = scrambled_file.len();
 
-    for index in 0..original_file.len() {
-        let value_to_move = original_file[index % original_file.len()];
+    for index in 0..len {
+        let test = match index {
+            0 => Some([1, 2, -3, 3, -2, 0, 4]),
+            1 => Some([2, 1, -3, 3, -2, 0, 4]),
+            2 => Some([1, -3, 2, 3, -2, 0, 4]),
+            3 => Some([1, 2, 3, -2, -3, 0, 4]),
+            4 => Some([1, 2, -2, -3, 0, 3, 4]),
+            5 => Some([1, 2, -3, 0, 3, 4, -2]),
+            6 => Some([1, 2, -3, 0, 3, 4, -2]),
+            7 => Some([1, 2, -3, 4, 0, 3, -2]),
+            _ => None,
+        };
+
+        if let Some(test) = test {
+            assert_eq!(scrambled_file, test);
+        }
+
+        let value_to_move = original_file[index % len];
         let src_index = scrambled_file
             .iter()
             .enumerate()
@@ -20,24 +53,38 @@ pub fn a(input: &str) -> i32 {
             .unwrap()
             .0;
 
-        let dst_index = {
-            let mut dst_index =
-                (src_index as i32 + value_to_move) % (original_file.len() as i32 - 1);
-            if dst_index <= 0 {
-                dst_index += original_file.len() as i32 - 1;
-            }
-            dst_index as usize
-        };
+        print!("{value_to_move} {src_index}: {:?} => ", scrambled_file);
 
-        match src_index.cmp(&dst_index) {
-            Ordering::Less => {
-                scrambled_file[src_index..=dst_index].rotate_left(1);
-            }
+        match value_to_move.cmp(&0) {
             Ordering::Greater => {
-                scrambled_file[dst_index..=src_index].rotate_right(1);
+                for count in 0..value_to_move {
+                    let from = modulo(src_index as i32 + count, len);
+                    let next = modulo(src_index as i32 + count + 1, len);
+                    scrambled_file.swap(from, next);
+                }
             }
-            Ordering::Equal => {}
+            Ordering::Less => {
+                let value_to_move = value_to_move.abs();
+                let mut needs_rotation = false;
+
+                for count in 0..value_to_move {
+                    let from = modulo(src_index as i32 - count, len);
+                    let next = modulo(src_index as i32 - count - 1, len);
+                    scrambled_file.swap(from, next);
+
+                    if next == 0 {
+                        needs_rotation = true;
+                    }
+                }
+
+                if needs_rotation {
+                    scrambled_file.rotate_left(1);
+                }
+            }
+            Ordering::Equal => (),
         }
+
+        println!("{:?}", scrambled_file);
     }
 
     let zero_index = scrambled_file
@@ -57,7 +104,7 @@ pub fn a(input: &str) -> i32 {
 #[test]
 fn test_a() {
     assert_eq!(a(TEST_INPUT), 3);
-    assert_eq!(a(INPUT), 0);
+    //assert_eq!(a(INPUT), 0);
 }
 
 pub fn b(input: &str) -> i32 {
