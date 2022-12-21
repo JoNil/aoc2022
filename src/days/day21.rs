cb7f1da01b4cac3ca08af8092afff3095e04b597
Jonathan Nilsson <jonathan@voysys.se>
2022-12-22 00:03:15 +0100

    Day 21 a

--- src/days/day21.rs
+++ src/days/day21.rs
@@ -1,8 +1,48 @@
+use std::collections::HashMap;
+
+use parse_display::FromStr;
+
 pub static INPUT: &str = include_str!("../input/21.txt");
 pub static TEST_INPUT: &str = include_str!("../input/21_test.txt");
 
-pub fn a(input: &str) -> i32 {
-    0
+#[derive(FromStr, Debug)]
+enum Operation {
+    #[display("{0}")]
+    Num(i32),
+    #[display("{1} {0} {2}")]
+    Op(char, String, String),
+}
+
+impl Operation {
+    fn eval(&self, refs: &HashMap<&str, Operation>) -> i128 {
+        match &self {
+            Operation::Num(val) => *val as i128,
+            Operation::Op(op, a, b) => {
+                let a = refs.get(a.as_str()).unwrap().eval(refs);
+                let b = refs.get(b.as_str()).unwrap().eval(refs);
+
+                match op {
+                    '+' => a + b,
+                    '-' => a - b,
+                    '*' => a * b,
+                    '/' => a / b,
+                    _ => panic!("Unknown op {op}"),
+                }
+            }
+        }
+    }
+}
+
+pub fn a(input: &str) -> i128 {
+    let ops = input
+        .lines()
+        .map(|line| {
+            let (name, op) = line.split_once(':').unwrap();
+            (name, op.trim().parse::<Operation>().unwrap())
+        })
+        .collect::<HashMap<_, _>>();
+
+    ops.get("root").unwrap().eval(&ops)
 }
 
 #[test]
